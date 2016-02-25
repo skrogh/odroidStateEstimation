f49e3a7ac36917ce9adb10f71c5a7c0e36119009
Søren Andersen  (emb.) <s123369@stuent.dtu.dk>
2016-02-25 18:44:42 +0100

    updated test program to output log of lsd slam and imu

--- src/test/main_test.cpp
+++ src/test/main_test.cpp
@@ -1,10 +1,62 @@
 #include <iostream>
-#include "imuDriver/src/ImuDriver.h"
+#include <fstream>
+#include "ImuDriver.h"
+
+#include "LiveSLAMWrapper.h"
+
+#include <boost/thread.hpp>
+#include "util/settings.h"
+#include "util/globalFuncs.h"
+#include "SlamSystem.h"
+
+
+#include "IOWrapper/OpenCV/OpenCVImageStreamThread.h"
+#include "IOWrapper/OpenCV/OpenCVOutput3DWrapper.h"
+#include "IOWrapper/ImageDisplay.h"
+
+
+
+void
+slamThreadFunction(lsd_slam::InputImageStream* inputStream, lsd_slam::Output3DWrapper* outputWrapper)
+{
+	lsd_slam::LiveSLAMWrapper slamNode(inputStream, outputWrapper);
+	slamNode.Loop();
+}
 
 int
-main(void)
+main( int argc, char** argv )
 {
+	lsd_slam::displayDepthMap = false;
+	
+	//open Logging file
+	std::ofstream imuFile;
+	imuFile.open("imuData.txt", std::ofstream::out);
+
+	// Start IMU grabber (spawns it's own thread)
 	std::cout << "Started" << std::endl;
 	ImuDriver imuDriver("/dev/spidev1.0","/sys/class/gpio/gpio199/value",100);
+	
+	// Start LSD SLAM (we spawn a tread of ir)
+	lsd_slam::InputImageStream* inputStream = new lsd_slam::OpenCVImageStreamThread();
+	inputStream->setCalibration("calib.txt");
+	inputStream->run();
+	lsd_slam::Output3DWrapper* outputWrapper = new lsd_slam::OpenCVOutput3DWrapper(inputStream->width(), inputStream->height());
+
+	boost::thread slamThread(slamThreadFunction, inputStream, outputWrapper);
+
+	// Main code goes here
+	while(1)
+	{
+		ImuMeas_t imuData;
+		imuDriver.imuBuffer.popBlocking(imuData);
+		imuFile << imuData.gyro[0] << " " << imuData.gyro[1] << " " << imuData.gyro[2] << " "
+		<< imuData.acc[0] << " " << imuData.acc[1] << " " << imuData.acc[2] << " "
+		<< std::chrono::duration_cast<std::chrono::nanoseconds>(imuData.timeStamp.time_since_epoch()).count()
+		<< std::endl;
+	}
+
+	// Exit code (logging? cleanup) goes here
+	slamThread.join(); // Join thread before exiting, or the lingering refs. to I/O wrappers will cause undefined behaviour
+	imuFile.close();
 	return 0;
 }
